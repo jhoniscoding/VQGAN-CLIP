89d59552bf860e5ca6689d138a21e293b0a77da2
Jhon Ramirez <jhoniscoding@gmail.com>
2022-01-27 06:14:44 -0500

    [ADD]: Adds initial scripts

---
+++ make.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+TEXT=$1
+NUM_IT=$2
+SIZE=600
+APE="ape.png"
+APEG="apeg.png"
+LION="lion.jpg"
+
+
+function generate {
+    echo "Generating NFT from $1..."
+    python generate.py -o ../fury_fda-labs-playground/output-$1 -i $NUM_IT -se $NUM_IT -ii samples/$1 -cutm original -cuts 128 -p "\"$TEXT\"" -s $SIZE $SIZE
+    echo "Generating NFT from $1... Done"
+}
+
+generate $LION
+generate $APE
+generate $APEG
